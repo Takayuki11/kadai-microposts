83fb853ab40bc42cebfde2c19cdff3fd70dfa8a1
taka <0bn644063264k0f@ezweb.ne.jp>
2020-08-31 07:08:42 +0000

    final homework

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,9 @@ class User < ApplicationRecord
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
   
+  has_many :favorites
+  has_many :find_favorites, through: :favorites, source: :micropost
+  
   
   def follow(other_user)
     unless self == other_user
@@ -30,5 +33,18 @@ class User < ApplicationRecord
   
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
+  end
+  
+  def favorite(other_micropost)
+      self.favorites.find_or_create_by(micropost_id: other_micropost.id)
+  end
+  
+  def unfavorite(other_micropost)
+    favorite = self.favorites.find_by(micropost_id: other_micropost.id)
+    favorite.destroy if favorite
+  end
+  
+  def favorite?(other_micropost)
+    self.find_favorites.include?(other_micropost)
   end
 end
